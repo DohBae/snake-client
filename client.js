17755bc0d6868167200e2fece55aa1efbc4b520b
DohBae <bdoherty92@msn.com>
2022-08-03 20:38:22 +0000

    Made snake move up once there aws a connection

--- client.js
+++ client.js
@@ -3,12 +3,21 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
+    host: "localhost", 
     port: 50541
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to the server");
+    conn.write("Name: BD");
+  });
+
+  conn.on('data', (data) => {
+    console.log("Move: up");
+  });
 
   conn.on('data', (data) => {
     console.log(data);
